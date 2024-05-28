c043f2a5fd7e71529a9315e7a005803e4c53f335
Nedy Nugroho <87101532+bwx1y@users.noreply.github.com>
2024-05-28 22:06:32 +0700

    tes

---
+++ src/DTO/login-request.js
@@ -0,0 +1,26 @@
+const { body, validationResult } = require("express-validator");
+
+const loginRequest = [
+  body("email").notEmpty().withMessage("email is required"),
+  body("password").notEmpty().withMessage("password is required"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((err) => err.msg);
+      return res.status(400).json({ message: errorMessages[0] });
+    }
+    next();
+  },
+];
+
+const generateToken = [
+  body("code").notEmpty().withMessage("password is required"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((err) => err.msg);
+      return res.status(400).json({ message: errorMessages[0] });
+    }
+    next();
+  },
+];
--- src/models/user.js
+++ src/models/user.js
@@ -1,50 +1,52 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('user', {
-    id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    username: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    token: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    role: {
-      type: DataTypes.ENUM('admin','siswa'),
-      allowNull: false,
-      defaultValue: "siswa"
-    },
-    code: {
-      type: DataTypes.STRING(5),
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    tableName: 'user',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
+const Sequelize = require("sequelize");
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    "user",
+    {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
       },
-    ]
-  });
+      username: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      token: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      role: {
+        type: DataTypes.ENUM("admin", "siswa"),
+        allowNull: false,
+        defaultValue: "siswa",
+      },
+      code: {
+        type: DataTypes.STRING(5),
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: "user",
+      timestamps: false,
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "id" }],
+        },
+      ],
+    }
+  );
 };
