c3000b91c7f0f86641789bae382739e823e3d6ab
Nedy Nugroho <87101532+bwx1y@users.noreply.github.com>
2024-06-03 22:04:18 +0700

    remove password

--- src/routes/Admin/AdminDataSiswaController.js
+++ src/routes/Admin/AdminDataSiswaController.js
@@ -2,55 +2,54 @@ const { Router } = require("express");
 const { Models } = require("../../models");
 const { where } = require("sequelize");
 
-const router = Router()
+const router = Router();
 
 router.put("/data-diri/:id", async (req, res) => {
+  const user_id = req.params.id;
+  const { ayah_kandung, ibu_kandung, data_diri, hobi, kesehatan, pendidikan, perkembangan, setelah_pendidikan, tempat_tinggal, wali } = req.body;
+  console.log(data_diri);
 
-    const user_id  = req.params.id;
-    const { ayah_kandung, ibu_kandung, data_diri, hobi, kesehatan, pendidikan, perkembangan, setelah_pendidikan, tempat_tinggal, wali } = req.body;
-    console.log(data_diri);
-  
-    try {
-      await Models.keterangan_ayah_kandung.update(ayah_kandung, { where: { user_id } });
-      await Models.keterangan_ibu_kandung.update(ibu_kandung, { where: { user_id } });
-      await Models.keterangan_data_diri.update(data_diri, { where: { user_id } });
-      await Models.keterangan_hobi_siswa.update(hobi, { where: { user_id } });
-      await Models.keterangan_kesehatan.update(kesehatan, { where: { user_id } });
-      await Models.keterangan_pendidikan.update(pendidikan, { where: { user_id } });
-      await Models.keterangan_perkembangan.update(perkembangan, { where: { user_id } });
-      await Models.keterangan_setelah_pendidikan.update(setelah_pendidikan, { where: { user_id } });
-      await Models.keterangan_tempat_tinggal.update(tempat_tinggal, { where: { user_id } });
-      await Models.keterangan_wali.update(wali, { where: { user_id } });
-  
-      return res.json({ message: "Data updated successfully" });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: "An error occurred while updating the data" });
-    }
-    
-})
+  try {
+    await Models.keterangan_ayah_kandung.update(ayah_kandung, { where: { user_id } });
+    await Models.keterangan_ibu_kandung.update(ibu_kandung, { where: { user_id } });
+    await Models.keterangan_data_diri.update(data_diri, { where: { user_id } });
+    await Models.keterangan_hobi_siswa.update(hobi, { where: { user_id } });
+    await Models.keterangan_kesehatan.update(kesehatan, { where: { user_id } });
+    await Models.keterangan_pendidikan.update(pendidikan, { where: { user_id } });
+    await Models.keterangan_perkembangan.update(perkembangan, { where: { user_id } });
+    await Models.keterangan_setelah_pendidikan.update(setelah_pendidikan, { where: { user_id } });
+    await Models.keterangan_tempat_tinggal.update(tempat_tinggal, { where: { user_id } });
+    await Models.keterangan_wali.update(wali, { where: { user_id } });
+
+    return res.json({ message: "Data updated successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "An error occurred while updating the data" });
+  }
+});
 
 router.get("/dashboard", async (req, res) => {
-  const count_siswa = await Models.user.count()
+  const count_siswa = await Models.user.count();
   const count_laki = await Models.keterangan_data_diri.count({
     where: {
-      jenis_kelamin : "laki-laki"
-    }
-  })
+      jenis_kelamin: "laki-laki",
+    },
+  });
 
   const count_perempuan = await Models.keterangan_data_diri.count({
-
     where: {
-      jenis_kelamin : "perempuan"
-    }
+      jenis_kelamin: "perempuan",
+    },
+  });
 
-  })
+  const count_datainputed = await Models.keterangan_data_diri.count();
 
-  const count_datainputed = await Models.keterangan_data_diri.count()
-  
   res.status(200).json({
-    count_datainputed, count_laki, count_perempuan, count_siswa
-  })
-})
+    count_datainputed,
+    count_laki,
+    count_perempuan,
+    count_siswa,
+  });
+});
 
-module.exports = router
+module.exports = router;
