309911d92f32c04e2d6c0612ac024d79729a4c91
Nedy Nugroho <87101532+bwx1y@users.noreply.github.com>
2024-06-10 17:00:52 +0700

    update angkatan

--- src/models/angkatan.js
+++ src/models/angkatan.js
@@ -1,29 +1,31 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('angkatan', {
-    id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    tahun: {
-      type: DataTypes.DATE(4),
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    tableName: 'angkatan',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
+const Sequelize = require("sequelize");
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    "angkatan",
+    {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
       },
-    ]
-  });
+      tahun: {
+        type: DataTypes.INTEGER(4),
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: "angkatan",
+      timestamps: false,
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "id" }],
+        },
+      ],
+    }
+  );
 };
