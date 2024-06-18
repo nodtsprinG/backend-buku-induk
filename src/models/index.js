4b32fe2b01f4881ac3a7a18b24a3e0c3cc6786d6
BigApple35 <drscity20@gmail.com>
2024-06-18 14:51:21 +0700

    dasdas

--- src/models/index.js
+++ src/models/index.js
@@ -7,6 +7,7 @@ module.exports = {
       host: "localhost",
       database: "db_aplikasi_buku_induk",
       username: "root",
+      password : "BigBlueApple",
       dialect: "mysql",
       port: 3306,
       logging: false,
