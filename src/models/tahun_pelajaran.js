3318d89e3824497da085d2272acfd830c8d84ff6
AnandaCahya <tgigroupjap@gmail.com>
2025-02-01 17:05:22 +0700

    feat [tahun_pelajaran] menambahkan model dan migration tahun_pelajaran

---
+++ src/models/tahun_pelajaran.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('tahun_pelajaran', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    nama: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: "tahun_pelajaran"
+    },
+  }, {
+    sequelize,
+    tableName: 'tahun_pelajaran',
+    timestamps: false,
+  });
+};
